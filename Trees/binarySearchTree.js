197bae5d3cc7cd67d96672c2b77db485093e3ab9
J.D. Browne <PeshwariNaan01@proton.me>
2023-02-02 12:56:43 +0100

    Added BFS and DFS to binarySearchTree.js.

--- Trees/binarySearchTree.js
+++ Trees/binarySearchTree.js
@@ -1,3 +1,5 @@
+// Import the queue class and see if I can implement the BFS with the queue I already have
+
 class Node {
   constructor(data) {
     this.data = data;
@@ -59,82 +61,162 @@ class BinarySearchTree {
     }
     let currentNode = this.root;
     let parentNode = null;
-    while(currentNode){
-      if(value < currentNode.value){
+    while (currentNode) {
+      if (value < currentNode.value) {
         parentNode = currentNode;
         currentNode = currentNode.left;
-      } else if(value > currentNode.value){
+      } else if (value > currentNode.value) {
         parentNode = currentNode;
         currentNode = currentNode.right;
       } else if (currentNode.value === value) {
         //We have a match, get to work!
-        
-        //Option 1: No right child: 
+
+        //Option 1: No right child:
         if (currentNode.right === null) {
           if (parentNode === null) {
             this.root = currentNode.left;
           } else {
-            
             //if parent > current value, make current left child a child of parent
-            if(currentNode.value < parentNode.value) {
+            if (currentNode.value < parentNode.value) {
               parentNode.left = currentNode.left;
-            
-            //if parent < current value, make left child a right child of parent
-            } else if(currentNode.value > parentNode.value) {
+
+              //if parent < current value, make left child a right child of parent
+            } else if (currentNode.value > parentNode.value) {
               parentNode.right = currentNode.left;
             }
           }
-        
-        //Option 2: Right child which doesnt have a left child
+
+          //Option 2: Right child which doesnt have a left child
         } else if (currentNode.right.left === null) {
           currentNode.right.left = currentNode.left;
-          if(parentNode === null) {
+          if (parentNode === null) {
             this.root = currentNode.right;
           } else {
-            
             //if parent > current, make right child of the left the parent
-            if(currentNode.value < parentNode.value) {
+            if (currentNode.value < parentNode.value) {
               parentNode.left = currentNode.right;
-            
-            //if parent < current, make right child a right child of the parent
+
+              //if parent < current, make right child a right child of the parent
             } else if (currentNode.value > parentNode.value) {
               parentNode.right = currentNode.right;
             }
           }
-        
-        //Option 3: Right child that has a left child
-        } else {
 
+          //Option 3: Right child that has a left child
+        } else {
           //find the Right child's left most child
           let leftmost = currentNode.right.left;
           let leftmostParent = currentNode.right;
-          while(leftmost.left !== null) {
+          while (leftmost.left !== null) {
             leftmostParent = leftmost;
             leftmost = leftmost.left;
           }
-          
+
           //Parent's left subtree is now leftmost's right subtree
           leftmostParent.left = leftmost.right;
           leftmost.left = currentNode.left;
           leftmost.right = currentNode.right;
 
-          if(parentNode === null) {
+          if (parentNode === null) {
             this.root = leftmost;
           } else {
-            if(currentNode.value < parentNode.value) {
+            if (currentNode.value < parentNode.value) {
               parentNode.left = leftmost;
-            } else if(currentNode.value > parentNode.value) {
+            } else if (currentNode.value > parentNode.value) {
               parentNode.right = leftmost;
             }
           }
         }
-      return true;
+        return true;
       }
     }
   }
+
+  // Inefficient solution using arrays as queues (Bad Big-O)
+  breadthFirstSearch() {
+    let currentNode = this.root;
+    let list = [];
+    let queue = [];
+
+    queue.push(currentNode);
+
+    while (queue.length > 0) {
+      currentNode = queue.shift(); //Shift returns the first item in the queue
+      console.log(currentNode.data);
+      list.push(currentNode.data);
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
+    return list;
+  }
+
+  // Breadth First search with recursion and arrays (still inefficient)
+  breadthFirstSearchRec(queue, list) {
+    if (!queue.length) {
+      return list;
+    }
+    let currentNode = queue.shift();
+    list.push(currentNode.data);
+    if (currentNode.left) {
+      queue.push(currentNode.left);
+    }
+    if (currentNode.right) {
+      queue.push(currentNode.right);
+    }
+    return this.breadthFirstSearchRec(queue, list);
+  }
+
+  DFSInOrder() {
+    return traverseInOrder(this.root, []);
+  }
+  DFSPostOrder() {
+    return traversePostOrder(this.root, []);
+  }
+  DFSPreOrder() {
+    return traversePreOrder(this.root, []);
+  }
 }
 
+function traverseInOrder(node, list) {
+  //console.log(node.data);
+  if (node.left) {
+    traverseInOrder(node.left, list);
+  }
+  list.push(node.data);
+  if (node.right) {
+    traverseInOrder(node.right, list);
+  }
+  return list;
+}
 
+function traversePreOrder(node, list) {
+  //console.log(node.data);
+  list.push(node.data);
+  if (node.left) {
+    traversePreOrder(node.left, list);
+  }
+  if (node.right) {
+    traversePreOrder(node.right, list);
+  }
+  return list;
+}
+
+function traversePostOrder(node, list) {
+  //console.log(node.data);
+
+  if (node.left) {
+    traversePostOrder(node.left, list);
+  }
+  if (node.right) {
+    traversePostOrder(node.right, list);
+  }
+  list.push(node.data);
+  return list;
+}
 
 const myTree = new BinarySearchTree();
 
@@ -153,4 +235,11 @@ myTree.insert(170);
 myTree.insert(15);
 myTree.insert(1);
 //console.log(JSON.stringify(traverse(myTree.root)));
-console.log(myTree.lookup(9));
+//console.log(myTree.lookup(9));
+//myTree.breadthFirstSearch();
+// console.log(myTree.breadthFirstSearch());
+// console.log(myTree.breadthFirstSearchRec([myTree.root], []));
+//myTree.DFSInOrder();
+console.log(myTree.DFSInOrder());
+console.log(myTree.DFSPreOrder());
+console.log(myTree.DFSPostOrder());
